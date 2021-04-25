0402d2f73282c8a71c8abca37b6a5dc7b146ac5e
minhdann.g <27481530+dann123456@users.noreply.github.com>
2021-04-25 15:35:23 +1000

    Add files via upload

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#from modules import *
+from modules import *
 import psycopg2
 
 #####################################################
@@ -12,8 +12,8 @@ Connects to the database using the connection string
 '''
 def openConnection():
 # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
-    userid = "y19s1c9120_mdin6872"
-    passwd = "pwd"
+    userid = "y18s2c9120_unikey"
+    passwd = "password"
     myHost = "soit-db-pro-2.ucc.usyd.edu.au"
 
     # Create a connection to the database
@@ -35,35 +35,12 @@ List all the user associated issues in the database for a given user
 See assignment description for how to load user associated issues based on the user id (user_id)
 '''
 def findUserIssues(user_id):
+    # TODO - list all user associated issues from db using sql
     print(user_id)
-    issue_db = []
-    try:
-        # fetch connection object to connect to the database
-        conn = openConnection()
-        # fetch cursor prepare to query the database
-
-        curs = conn.cursor()
-        curs.execute("BEGIN;")
-        curs.callproc('listIssue', [int(user_id)])
-
-        # loop through the resultset #/
-        nr = 0
-        row = curs.fetchone()
-
-        while row is not None:
-            nr+=1
-            # convert to string for web display
-            convert = list(map(str, row))
-            issue_db.append(convert)
-            row = curs.fetchone()
-          
-        if ( nr == 0 ):
-            print("No entries found.")
-
-    except psycopg2.Error as sqle:
-
-        #TODO: add error handling #/
-        print("psycopg2.Error : " + sqle.pgerror)
+    issue_db = [
+        ['Division by zero', '1', '1', '1', 'Division by 0 doesn\'t yield error or infinity as would be expected. Instead it results in -1.', '1'],
+        ['Factorial with addition anomaly', '1', '1', '1', 'No description', '2']
+    ]
 
     issue = [{
         'title': row[0],
@@ -74,10 +51,7 @@ def findUserIssues(user_id):
         'issue_id': row[5]
     } for row in issue_db]
 
-    curs.close()
-    conn.close()
     return issue
-
 
 '''
 Find the associated issues for the user with the given userId (user_id) based on the searchString provided as the parameter, and based on the assignment description
@@ -86,32 +60,10 @@ def findIssueBasedOnExpressionSearchedOnTitleAndDescription(searchString, user_i
 
     # TODO - find necessary issues using sql database based on search input
     print(user_id)
-    searchString = "%"+searchString+"%"
-    print(searchString)
-    
-    issue_db = []
-
-    try:
-        # fetch connection object to connect to the database
-        conn = openConnection()
-        # fetch cursor prepare to query the database
-
-        curs = conn.cursor()
-        # execute a SQL query
-        sql = """SELECT * FROM a3_issue WHERE (title LIKE %s or description LIKE %s) 
-                 and (creator = %s or resolver = %s or verifier = %s) 
-                 ORDER BY title ASC"""
-        data = (searchString, searchString, user_id, user_id, user_id)
-        curs.execute(sql, data)
-        
-        # can loop through the resultset
-        print(curs)
-        for result in curs:
-            issue_db.append(list(result))
-            
-    except psycopg2.Error as sqle:       
-        #TODO: add error handling #/
-        print("psycopg2.Error : " + sqle.pgerror)
+    print("search string '" + searchString + "'")
+    issue_db = [
+        ['Division by zero', '1', '1', '1', 'Division by 0 doesn\'t yield error or infinity as would be expected. Instead it results in -1.', '1']
+    ]
 
     issue = [{
         'title': row[0],
@@ -122,8 +74,6 @@ def findIssueBasedOnExpressionSearchedOnTitleAndDescription(searchString, user_i
         'issue_id': row[5]
     } for row in issue_db]
 
-    curs.close()
-    conn.close()
     return issue
 
 #####################################################
@@ -136,28 +86,6 @@ def addIssue(title, creator, resolver, verifier, description):
     # return False if adding was unsuccessful 
     # return Ture if adding was successful
 
-    try:
-        # fetch connection object to connect to the database
-        conn = openConnection()
-        # fetch cursor prepare to query the database
-
-        curs = conn.cursor()
-
-        sql = """INSERT INTO a3_issue (title, creator, resolver, verifier, description) VALUES (%s, %s, %s, %s, %s )"""
-        data = (title, creator, resolver, verifier, description)
-
-        curs.execute(sql, data)
-        conn.commit()
-    
-    except psycopg2.Error as sqle:       
-        #TODO: add error handling #/
-        print("psycopg2.Error : " + sqle.pgerror)
-
-        conn.rollback()
-        return False
-
-    curs.close()
-    conn.close()
     return True
 
 #Update the details of an issue having the provided issue_id with the values provided as parameters
@@ -167,27 +95,4 @@ def updateIssue(title, creator, resolver, verifier, description, issue_id):
 
     # return False if adding was unsuccessful 
     # return Ture if adding was successful
-     
-    try:
-        # fetch connection object to connect to the database
-        conn = openConnection()
-        # fetch cursor prepare to query the database
-        curs = conn.cursor()
-
-        sql = """UPDATE a3_issue SET title = %s, creator = %s, resolver= %s, verifier= %s, description=%s WHERE issue_id = %s """
-        data = (title, creator, resolver, verifier, description, issue_id)
-
-        curs.execute(sql, data)
-        conn.commit()
-    
-    except psycopg2.Error as sqle:       
-        #TODO: add error handling #/
-        print("psycopg2.Error : " + sqle.pgerror)
-
-        conn.rollback()
-        return False
-
-    curs.close()
-    conn.close()
-
     return True
